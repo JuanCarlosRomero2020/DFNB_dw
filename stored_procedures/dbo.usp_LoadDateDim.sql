787d98ece01ffbd476a2de1554ac665d4ae76ee4
Juan Carlos Romero <jcrr2@ensing.edu>
2021-03-20 23:11:04 -0600

    IT 243 - Added Date Dimension enhancement

--- stored_procedures/dbo.usp_LoadDateDim.sql
+++ stored_procedures/dbo.usp_LoadDateDim.sql
@@ -120,37 +120,82 @@ SELECT CONVERT(VARCHAR, (@v_first_date + n.n), 112) AS date_id
      , CONVERT(VARCHAR, DATEADD(dd, -(DATEPART(dw, (@v_first_date + n.n)) - 1), (@v_first_date + n.n)), 112) AS week_start_date_id
      , FORMAT(DATEADD(dd, 7 - DATEPART(dw, (@v_first_date + n.n)), (@v_first_date + n.n)), 'yyyy-MM-dd') AS week_end_date
      , CONVERT(VARCHAR, DATEADD(dd, 7 - DATEPART(dw, (@v_first_date + n.n)), (@v_first_date + n.n)), 112) AS week_end_date_id
-     , NULL AS weekday_flag
-     , NULL AS weekend_flag
-     , NULL AS last_day_in_week_flag
-     , NULL AS month_number
-     , NULL AS month_name
-     , NULL AS month_abbreviation
+
+     , CASE WHEN DATEPART( dw, (@v_first_date + n.n )) 
+	   IN (7,1) THEN 0
+	   ELSE 1
+	   END AS weekday_flag
+
+     , CASE WHEN DATEPART( dw, (@v_first_date + n.n )) 
+	   IN (7,1) THEN 1
+	   ELSE 0
+	   END  AS weekend_flag
+     , CASE WHEN DATEPART( dw, (@v_first_date + n.n )) 
+	   IN (7) THEN 1
+	   ELSE 0
+	   END  AS last_day_in_week_flag
+
+     , DATEPART( mm, (@v_first_date + n.n )) AS month_number
+     , DATENAME(MONTH, (@v_first_date + n.n)) AS month_name
+     , FORMAT((@v_first_date + n.n), 'MMM')  AS month_abbreviation
+
      , NULL AS month_last_day_number
-     , NULL AS month_start_date
+
+     , DATEADD(DAY,1,EOMONTH((@v_first_date + n.n),-1)) AS month_start_date
+	     
      , NULL AS month_start_date_id
-     , NULL AS month_end_date
+
+     , DATEADD (dd, -1, DATEADD(mm, DATEDIFF(mm, 0, (@v_first_date + n.n)) +1, 0)) AS month_end_date
+
      , NULL AS month_end_date_id
-     , NULL AS month_end_date_previous
+
+     , DATEADD(dd, -1, DATEADD(mm, DATEDIFF(mm, 0, (@v_first_date + n.n)), 0)) AS month_end_date_previous
+
      , NULL AS month_end_date_previous_id
-     , NULL AS last_day_in_month_flag
-     , NULL AS quarter_number
-     , NULL AS quarter_name
-     , NULL AS quarter_code
-     , NULL AS quarter_start_date
+
+     , CASE WHEN DATEPART( dy, (@v_first_date + n.n )) 
+	   IN (31,59,90,120,151,181,212,243,273,304,334,365) THEN 1
+	   ELSE 0
+	   END  AS last_day_in_month_flag
+     , DATEPART( qq, (@v_first_date + n.n )) AS quarter_number
+     , CASE WHEN DATEPART(quarter , (@v_first_date + n.n )) = DATEPART(quarter, 1) THEN 'Q1'
+	        WHEN DATEPART(quarter , (@v_first_date + n.n )) = DATEPART(quarter, 2) THEN 'Q2'
+			WHEN DATEPART(quarter , (@v_first_date + n.n )) = DATEPART(quarter, 3) THEN 'Q3'
+			WHEN DATEPART(quarter , (@v_first_date + n.n )) = DATEPART(quarter, 4) THEN 'Q4'
+       END  AS quarter_name
+
+	 , NULL AS quarter_code
+
+     , DATEADD(qq, DATEDIFF(qq, 0, (@v_first_date + n.n)), 0) AS quarter_start_date
+
      , NULL AS quarter_start_date_id
-     , NULL AS quarter_end_date
+
+     , DATEADD (dd, -1, DATEADD(qq, DATEDIFF(qq, 0, (@v_first_date + n.n)) +1, 0)) AS quarter_end_date
+
      , NULL AS quarter_end_date_id
-     , NULL AS last_day_in_quarter_flag
-     , NULL AS year_number
-     , NULL AS year_start_date
+
+     , CASE WHEN DATEPART( dy, (@v_first_date + n.n )) 
+	   IN (90,181,273,365) THEN 1
+	   ELSE 0
+	   END AS last_day_in_quarter_flag
+     , DATEPART ( yy, (@v_first_date + n.n )) AS year_number
+     , DATEADD(yy, DATEDIFF(yy, 0, (@v_first_date + n.n)), 0) AS year_start_date
+
      , NULL AS year_start_date_id
-     , NULL AS year_end_date
+
+     , DATEADD (dd, -1, DATEADD(yy, DATEDIFF(yy, 0, (@v_first_date + n.n)) +1, 0)) AS year_end_date
+
      , NULL AS year_end_date_id
      , NULL AS yyyymm
-     , NULL AS last_day_in_year_flag
+
+     , CASE WHEN DATEPART( dy, (@v_first_date + n.n )) 
+	   IN (365) THEN 1
+	   ELSE 0
+	   END AS last_day_in_year_flag
+
      , NULL AS holiday_ind -- Challenge field
      , NULL AS holiday_name -- Challenge field
+
   FROM dbo.Nums AS n
 --  WHERE n.n <= (365 * @v_num_years)
   WHERE n.n <= (@v_num_days + 1) 
